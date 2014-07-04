ce9c9497060776907329b386365ae5ba713689f8
sujay <sujaybhowmick@gmail.com>
2014-07-04 14:32:40 +0530

    removed generic

--- src/main/java/org/algorithms/hashing/LookupMap.java
+++ src/main/java/org/algorithms/hashing/LookupMap.java
@@ -3,7 +3,7 @@ package org.algorithms.hashing;
 /**
  * Created by sujay on 7/4/14.
  */
-public interface LookupMap<T> {
+public interface LookupMap {
     void add(Object value);
     boolean contains(Object value);
     int size();
